119dbb97aea861011e3e9f2d0b1c6fb543fb8598
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2022-08-07 19:50:49 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"))
+
+const {v4:uuidv4}=require("uuid")
+
+app.get("/", (req, res) => {
+    // res.status(200).send("The fitness grand pacer test is a multi step arobic test");
+    res.redirect(`/${uuidv4()}`)
+});
+
+app.get("/:room",(req,res)=>{
+    res.render("index",{roomId:req.params.room})
+})
+
+server.listen(3030);
